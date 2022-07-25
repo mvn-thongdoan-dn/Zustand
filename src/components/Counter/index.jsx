a228aa45f430cbfb32fa3f0bbcf0734e6ff589e5
Thong Doan <thong.doan@monstar-lab.com>
2022-07-25 08:36:20 +0700

    edit file name

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import useStore from '../../store/store';
+import useStore from '../../store/countStore';
 
 const Counter = () => {
   const { count, incre, decre } = useStore((state) => state);
