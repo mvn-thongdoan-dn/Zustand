598f1e18568e18e0fc73a5e3cbe8c3628e4a4dc6
Thong Doan <thong.doan@monstar-lab.com>
2022-07-25 09:17:05 +0700

    create two store

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import shallow from 'zustand/shallow';
-import { bearStore, useStore } from '../../store/store';
+import bearStore from '../../store/bearStore';
+import countStore from '../../store/countStore';
 
 const Counter = () => {
-  const { count, incre, decre } = useStore((state) => ({
+  const { count, incre, decre } = countStore((state) => ({
     count: state.count,
     incre: state.incre,
     decre: state.decre
   }), shallow);
 
-  const { bear, increBear, getState } = bearStore((state) => ({
+  const { bear, increBear } = bearStore((state) => ({
     bear: state.bear,
     increBear: state.increBear,
-    getState: state.getState
   }))
 
   return (
+    <>
     <div className='counter'>
       <h1 className='counter-title'>Zustand Counter:</h1>
       <div className="btn-group">
@@ -23,11 +24,15 @@ const Counter = () => {
         <button className='btn btn-warning'>{count}</button>
         <button onClick={() => decre("decre")} className='btn btn-danger'>Decrease</button>
       </div>
-      <div className="btn-group" style={{display: "block", margin: "50px auto"}}>
-        <button onClick={() => getState()} className='btn btn-primary'>IncreBear</button>
+    </div>
+    <div className='counter'>
+      <h1 className='counter-title'>Zustand Bear:</h1>
+      <div className="btn-group">
+        <button onClick={() => increBear("incre")} className='btn btn-primary'>Increase Bear</button>
         <button className='btn btn-warning'>{bear}</button>
       </div>
     </div>
+    </>
   )
 }
 
---
+++ src/store/countStore.js
@@ -0,0 +1,28 @@
+import create from "zustand";
+import { devtools } from 'zustand/middleware';
+
+let countStore = (set) => ({
+  count: 0,
+
+  incre: (actionType) => set((state) => {
+    return {
+      count: state.count + 1
+    }
+  },
+  false,
+  `count/${actionType}`
+  ),
+
+  decre: (actionType) => set((state) => {
+    return {
+      count: state.count - 1
+    }
+  },
+  false,
+  `count/${actionType}`
+  )
+});
+
+countStore = devtools(countStore);
+
+export default create(countStore);
