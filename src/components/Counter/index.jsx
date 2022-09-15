00221f074c10c48c30d74a9f9af9e49766b6b6e1
Thong Doan <thong.doan@monstar-lab.com>
2022-09-15 21:00:21 +0700

    edit shallow

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -9,17 +9,15 @@ const Counter = () => {
     decre: state.decre
   }), shallow);
 
-  console.log("count", count);
-  console.log("incre", incre);
-  console.log("decre", decre);
+  console.log("count", count);  // result => 1;
 
   return (
     <div className='counter'>
       <h1 className='counter-title'>Zustand Counter:</h1>
       <div className="btn-group">
-        <button onClick={() => incre()} className='btn btn-primary'>Increase</button>
+        <button onClick={incre} className='btn btn-primary'>Increase</button>
         <button className='btn btn-warning'>{count}</button>
-        <button onClick={() => decre()} className='btn btn-danger'>Decrease</button>
+        <button onClick={decre} className='btn btn-danger'>Decrease</button>
       </div>
     </div>
   )
--- src/store/countStore.js
+++ src/store/countStore.js
@@ -3,11 +3,7 @@ import create from "zustand";
 const countStore = (set) => ({
   count: 0,
   incre: () => set({count: 1}),
-  decre: () => set((state) => {
-      return {
-        count: state.count - 1
-      }
-    })
+  decre: () => set((state) => ({count: state.count - 1}))
 });
 
 export default create(countStore);
